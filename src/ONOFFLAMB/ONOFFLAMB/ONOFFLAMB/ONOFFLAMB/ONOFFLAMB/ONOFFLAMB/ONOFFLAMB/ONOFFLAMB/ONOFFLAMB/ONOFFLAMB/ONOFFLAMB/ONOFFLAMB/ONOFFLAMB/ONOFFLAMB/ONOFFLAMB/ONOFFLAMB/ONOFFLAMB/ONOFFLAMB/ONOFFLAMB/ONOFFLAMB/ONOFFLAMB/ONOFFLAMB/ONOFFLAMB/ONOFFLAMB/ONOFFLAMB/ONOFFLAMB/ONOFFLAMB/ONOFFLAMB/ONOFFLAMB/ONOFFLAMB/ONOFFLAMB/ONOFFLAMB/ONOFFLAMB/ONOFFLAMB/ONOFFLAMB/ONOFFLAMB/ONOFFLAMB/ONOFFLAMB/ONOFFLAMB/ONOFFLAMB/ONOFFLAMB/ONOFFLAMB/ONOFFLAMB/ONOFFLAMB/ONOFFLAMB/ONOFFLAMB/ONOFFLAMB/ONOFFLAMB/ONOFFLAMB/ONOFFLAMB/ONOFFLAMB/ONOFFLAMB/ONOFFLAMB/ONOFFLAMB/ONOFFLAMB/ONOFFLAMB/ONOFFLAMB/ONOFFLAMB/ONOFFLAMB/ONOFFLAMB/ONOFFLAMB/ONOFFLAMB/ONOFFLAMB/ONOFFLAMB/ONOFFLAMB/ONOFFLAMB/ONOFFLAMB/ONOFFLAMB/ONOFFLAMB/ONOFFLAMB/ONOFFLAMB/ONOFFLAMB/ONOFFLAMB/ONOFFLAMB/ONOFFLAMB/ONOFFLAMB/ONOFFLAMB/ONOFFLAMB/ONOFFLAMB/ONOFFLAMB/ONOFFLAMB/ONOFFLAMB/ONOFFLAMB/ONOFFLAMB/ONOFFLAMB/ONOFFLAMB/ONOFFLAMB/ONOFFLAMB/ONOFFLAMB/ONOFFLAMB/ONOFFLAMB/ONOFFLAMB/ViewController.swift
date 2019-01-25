3ae32ab683b373243c1823cbabbb141644070904
nam228 <nampt282@gmail.com>
2019-01-25 05:31:35 +0100

    Initial Code for Speech To Text

---
+++ src/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ONOFFLAMB/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  ONOFFLAMB
+//
+//  Created by KID on 23/1/19.
+//  Copyright © 2019 NAM PHAM. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class ViewController: UIViewController,SFSpeechRecognizerDelegate {
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))!  //1
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    @IBOutlet var textView: UITextView!
+    
+    @IBOutlet var microphoneButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        microphoneButton.isEnabled = false  //2
+        
+        speechRecognizer.delegate = self  //3
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
+    }
+    
+    @IBAction func microphoneTapped(_ sender: AnyObject) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+            
+        }
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.record, mode: .measurement)
+            try audioSession.setActive(true,options: .notifyOthersOnDeactivation)
+            textView.text = "Audio Session Start."
+        } catch {
+            textView.text = "Audio Session Error..."
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+    }
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
+    }
+
+}
+
